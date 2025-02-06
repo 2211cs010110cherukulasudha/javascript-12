ee330d62b060714194f379ae22cfecca9b6255b6
2211cs010110cherukulasudha <2211cs010110@mallareddyuniversity.ac.in>
2025-02-06 10:21:28 +0530

    Create react.js

---
+++ react.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '/App.css';
+const Welcome=()=><h1 className='message' >hello sai</h1>
+function App() {
+  return(
+    <div className='App'>
+      <Welcome/>
+    </div>
+  )
+}
+export default App;
